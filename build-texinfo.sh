6e0882ff0553208739f5cfc5299a907cad23b41c
Raymond Toy <toy.raymond@gmail.com>
2019-06-01 09:00:30 -0700

    Fix script typos and change texi to build docs

--- build-texinfo.sh
+++ build-texinfo.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-# If the bin directory exists, assume we have everything we need and
-# don't try to download and build texinfo.
+# If the texinfo directory doesn't exist, clone it. Otherwise just
+# update the repo.
+if [ -d texinfo ]; then
+   cd texinfo
+   git stash; git stadh drop
+   git pull --rebase
+else
+   git clone https://git.savannah.gnu.org/git/texinfo.git
+   cd texinfo
+fi
+# Check out the desired version
+git checkout 63d8c9102dcb454842330f7477f5c7e38b938aba
 
-if [ ! -d bin]; then
-    if [ ! -d texinfo ]; then
-      git clone https://git.savannah.gnu.org/git/texinfo.git
-      cd texinfo
-   else
-      cd texinfo
-      git stash; git stadh drop
-   fi
-   # Check out the desired version
-   git checkout 63d8c9102dcb454842330f7477f5c7e38b938aba
+# If the bin directory doesn't exist, assume we need to build and
+# install texinfo
+
+if [ ! -d ../bin ]; then
    ./autogen.sh
    ./configure --prefix=$PWD/..
    make; make install
