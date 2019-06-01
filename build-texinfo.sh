640b9310e286ad141941f1847358a185d520941c
Raymond Toy <toy.raymond@gmail.com>
2019-06-01 13:00:39 -0700

    Some cleanups
    * Clean up build-texinfo a bit, and set -x so we can see what the script is doing.
    * Undo the changes to cmu-user.texi since things appear to be working now.

--- build-texinfo.sh
+++ build-texinfo.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# Show what we're doing.
+set -x
+
 # If the texinfo directory doesn't exist, clone it. Otherwise just
 # update the repo.
 if [ -d texinfo ]; then
    cd texinfo
    git stash; git stash drop
-   git pull --rebase
 else
    git clone https://git.savannah.gnu.org/git/texinfo.git
    cd texinfo
@@ -15,7 +17,6 @@ git checkout 63d8c9102dcb454842330f7477f5c7e38b938aba
 
 # If the bin directory doesn't exist, assume we need to build and
 # install texinfo
-
 if [ ! -d ../bin ]; then
    ./autogen.sh
    ./configure --prefix=$PWD/..
