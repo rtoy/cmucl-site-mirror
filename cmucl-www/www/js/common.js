db51144b4293563b8c66beea7a0ad1baeab1e8b5
Raymond Toy <toy.raymond@gmail.com>
2020-03-05 18:45:28 -0800

    Mobile navbar
    Based on https://codepen.io/dannievinther/pen/NvZjvz
    
    Not quite functional.  Hamburger is in the wrong place and menu items
    open up in the wrong place and you can't click the close button
    without also navigating somewhere.

--- cmucl-www/www/js/common.js
+++ cmucl-www/www/js/common.js
@@ -116,6 +116,61 @@ html`
       </li>
     </ol>
   </nav>
+  <nav id="mobile-navlist"
+       class="mobile-navlist">
+    <div class="bg"></div>
+    <div class="button" tabindex="0">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+</div>
+    <div id="mobile-nav-content" tabindex="0">
+    <ol id="mobile-navitems" style="list-style-type: none; text-align: center; padding: 0; margin: 0">
+      <li id="nav-faq">
+        <a href="${prefix}FAQ.html">FAQ</a>
+      </li>
+      <li id="nav-benchmarks">
+        <a href="${prefix}benchmarks/index.html">Benchmarks</a>
+      </li>
+      <li id="nav-credits">
+        <a href="${prefix}credits.html">Credits</a>
+      </li>
+      <li id="nav-documentation">
+        <a href="${prefix}doc/index.html">Documentation</a>
+      </li>
+      <li id="nav-download">
+        <a href="${prefix}download.html">Download</a>
+      </li>
+      <li id="nav-hemlock">
+        <a href="${prefix}hemlock/index.html">Hemlock</a>
+      </li>
+      <li id="nav-home">
+        <a href="${prefix}index.html">Home</a>
+      </li>
+      <li id="nav-install">
+        <a href="${prefix}install.html">Install</a>
+      </li>
+      <li id="nav-news">
+        <a href="${prefix}news/index.html">News</a>
+      </li>
+      <li id="nav-platforms">
+        <a href="${prefix}platforms.html">Platforms</a>
+      </li>
+      <li id="nav-ports">
+        <a href="${prefix}ports.html">Ports</a>
+      </li>
+      <li id="nav-projects">
+        <a href="${prefix}projects.html">Projects</a>
+      </li>
+      <li id="nav-search">
+        <a href="${prefix}search.html">Search</a>
+      </li>
+      <li id="nav-support">
+        <a href="${prefix}support.html">Support</a>
+      </li>
+</ol>
+</div>
+  </nav>
 `;
 
 render(headerTemplate(), document.getElementById('header'));
