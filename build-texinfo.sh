80818c7bf280170860aeb5ad601fd61ed0b70bd9
Raymond Toy <toy.raymond@gmail.com>
2025-11-26 12:59:51 -0800

    Allow building master branch of texinfo

--- build-texinfo.sh
+++ build-texinfo.sh
@@ -4,10 +4,18 @@
 set -x
 
 # Download the desired texinfo tarball for the version we want.
-VERSION=7.2
-wget https://ftp.gnu.org/gnu/texinfo/texinfo-$VERSION.tar.xz
-tar xf texinfo-$VERSION.tar.xz
+VERSION=master
 
+if [ "$VERSION" = "master"]; then
+    if [ -d "texinfo-$VERSION" ]; then
+        (cd texinfo-$VERSION; git checkout master)
+    else
+        git clone https://git.savannah.gnu.org/git/texinfo.git texinfo-$VERSION
+    fi
+else
+    wget https://ftp.gnu.org/gnu/texinfo/texinfo-$VERSION.tar.xz
+    tar xf texinfo-$VERSION.tar.xz
+fi
 # cd to the sources and build.
 cd texinfo-$VERSION
 ./configure --prefix=$PWD/..
