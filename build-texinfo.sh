5531bc2f6d8a6e938e2772b87cc970a91e4eca02
Raymond Toy <toy.raymond@gmail.com>
2025-11-28 13:03:11 -0800

    Use curl instead of wget to get texinfo sources

--- build-texinfo.sh
+++ build-texinfo.sh
@@ -5,7 +5,8 @@ set -x
 
 # Download the desired texinfo tarball for the version we want.
 VERSION=7.2
-wget https://ftp.gnu.org/gnu/texinfo/texinfo-$VERSION.tar.xz
+#wget https://ftp.gnu.org/gnu/texinfo/texinfo-$VERSION.tar.xz
+curl https://ftp.gnu.org/gnu/texinfo/texinfo-$VERSION.tar.xz > texinfo-$VERSION.tar.xz
 tar xf texinfo-$VERSION.tar.xz
 
 # cd to the sources and build.
