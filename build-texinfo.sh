c9e9ed9b5a66bd5933b4d7a293026c27fc20b946
Raymond Toy <toy.raymond@gmail.com>
2025-12-19 14:42:35 -0800

    Untar or clone to the texinfo dir
    Use a fixed directory for the texinfo builds.

--- build-texinfo.sh
+++ build-texinfo.sh
@@ -12,17 +12,18 @@ set -x
 # Download the desired texinfo tarball for the version we want.
 VERSION=dev
 if [ "$VERSION" = "dev" ]; then
-    git clone https://git.savannah.gnu.org/git/texinfo.git texinfo-$VERSION
+    git clone https://git.savannah.gnu.org/git/texinfo.git
 else
     # Not a development version so just download the sources.  These
     # untar to the directory texinfo-$VERSION.
-    wget https://ftp.gnu.org/gnu/texinfo/texinfo-$VERSION.tar.xz
+    wget --quiet https://ftp.gnu.org/gnu/texinfo/texinfo-$VERSION.tar.xz
     #curl https://ftp.gnu.org/gnu/texinfo/texinfo-$VERSION.tar.xz > texinfo-$VERSION.tar.xz
     tar xf texinfo-$VERSION.tar.xz
+    mv texinfo-$VERSION texinfo
 fi
 
 # cd to the sources and build.
-cd texinfo-$VERSION
+cd texinfo
 ./configure --prefix=$PWD/..
 make clean
 make
