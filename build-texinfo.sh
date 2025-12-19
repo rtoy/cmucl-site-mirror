99d450f32f97313ee2d74592853437f46416f5cf
Raymond Toy <toy.raymond@gmail.com>
2025-12-19 14:38:51 -0800

    Allow building the dev version of texinfo.

--- build-texinfo.sh
+++ build-texinfo.sh
@@ -4,14 +4,22 @@ echo PATH = $PATH
 type -all makeinfo
 makeinfo --version
 
+
+
 # Show what we're doing.
 set -x
 
 # Download the desired texinfo tarball for the version we want.
-VERSION=7.2
-wget https://ftp.gnu.org/gnu/texinfo/texinfo-$VERSION.tar.xz
-#curl https://ftp.gnu.org/gnu/texinfo/texinfo-$VERSION.tar.xz > texinfo-$VERSION.tar.xz
-tar xf texinfo-$VERSION.tar.xz
+VERSION=dev
+if [ "$VERSION" = "dev" ]; then
+    git clone https://git.savannah.gnu.org/git/texinfo.git texinfo-$VERSION
+else
+    # Not a development version so just download the sources.  These
+    # untar to the directory texinfo-$VERSION.
+    wget https://ftp.gnu.org/gnu/texinfo/texinfo-$VERSION.tar.xz
+    #curl https://ftp.gnu.org/gnu/texinfo/texinfo-$VERSION.tar.xz > texinfo-$VERSION.tar.xz
+    tar xf texinfo-$VERSION.tar.xz
+fi
 
 # cd to the sources and build.
 cd texinfo-$VERSION
