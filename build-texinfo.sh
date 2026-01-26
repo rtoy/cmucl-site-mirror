91cce3f585cde50e7abc8976cf4ea0bbfebef399
Raymond Toy <toy.raymond@gmail.com>
2026-01-25 20:19:41 -0800

    Built texinfo from the 7.2.90 tarball
    This version should have the syntax highlighting support we want.
    Let's try it.

--- build-texinfo.sh
+++ build-texinfo.sh
@@ -4,8 +4,12 @@
 set -x
 
 # Download the desired texinfo tarball for the version we want.
-VERSION=7.2
-wget https://ftp.gnu.org/gnu/texinfo/texinfo-$VERSION.tar.xz
+VERSION=7.2.90
+#wget https://ftp.gnu.org/gnu/texinfo/texinfo-$VERSION.tar.xz
+#
+# 7.3 isn't released yet but the test version 7.2.90 is available.
+# Download it from the alpha site.
+wget --quiet https://alpha.gnu.org/gnu/texinfo/texinfo-$VERSION.tar.xz
 tar xf texinfo-$VERSION.tar.xz
 
 # cd to the sources and build.
